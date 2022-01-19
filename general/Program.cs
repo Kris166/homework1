fb6d7e95b0e49680d7560daec6e4b542abdd8dfa
primas <primas.petrov@gmail.com>
2022-01-19 18:25:45 +0200

    Formating the code.

--- general/Program.cs
+++ general/Program.cs
@@ -6,31 +6,30 @@ namespace general
     {
         static void Main(string[] args)
         {
+            double vacation = double.Parse(Console.ReadLine());
+            int puzzle = int.Parse(Console.ReadLine());
+            int dolls = int.Parse(Console.ReadLine());
+            int bears = int.Parse(Console.ReadLine());
+            int minions = int.Parse(Console.ReadLine());
+            int trucks = int.Parse(Console.ReadLine());
+
+            int toys = puzzle + dolls + bears + minions + trucks;
+            double totalPrise = trucks * 2.6 + dolls * 3 + bears * 4.1 + minions * 8.2 + trucks * 2;
+
+            if (toys >= 50)
             {
-
-                double vacation = double.Parse(Console.ReadLine());
-                int puzzle = int.Parse(Console.ReadLine());
-                int dolls = int.Parse(Console.ReadLine());
-                int bears = int.Parse(Console.ReadLine());
-                int minions = int.Parse(Console.ReadLine());
-                int trucks = int.Parse(Console.ReadLine());
-
-                int toys = puzzle + dolls + bears + minions + trucks;
-                double totalPrise = trucks * 2.6 + dolls * 3 + bears * 4.1 + minions * 8.2 + trucks * 2;
-
-                if (toys >= 50)
-                {
-                    totalPrise = totalPrise - totalPrise * 0.25;
-                }
-                totalPrise = totalPrise - totalPrise * 0.1;
+                totalPrise = totalPrise - totalPrise * 0.25;
             }
-            if (totalPrise >= vacantion)
+            totalPrise = totalPrise - totalPrise * 0.1;
+            
+            if (totalPrise >= vacation)
             {
                 Console.WriteLine($"Yes! {totalPrise}lv left.");
-
             }
             else
-                Console.WriteLine($"Not enough money! {vacantion - totalPrise:F2} lv needed.");
+            {
+                Console.WriteLine($"Not enough money! {vacation - totalPrise:F2} lv needed.");
+            }
         }
     }
 }
